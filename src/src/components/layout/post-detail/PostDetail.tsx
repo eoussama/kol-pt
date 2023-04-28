92ee57501479dc503f93d56c4e06740d4245aaa0
EOussama <oussama_gtacj@live.fr>
2023-04-28 17:54:43 +0000

    Accordion styling #27

--- src/src/components/layout/post-detail/PostDetail.tsx
+++ src/src/components/layout/post-detail/PostDetail.tsx
@@ -1,17 +1,40 @@
 import styles from './PostDetail.module.scss';
 
-import { useMemo } from 'react';
+import { useState } from 'react';
 import { Post } from '../../../core/models/post.model';
-import { TimeHelper } from '../../../core/helpers/parse/time.helper';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import MuiAccordion, { AccordionProps } from '@mui/material/Accordion';
+import { AccordionDetails, AccordionSummary, styled } from '@mui/material';
 
 
+
+const Accordion = styled((props: AccordionProps) => (
+  <MuiAccordion disableGutters elevation={0} square {...props} />
+))(({ theme }) => ({
+  border: `1px solid ${theme.palette.divider}`,
+  '&:not(:last-child)': {
+    borderBottom: 0,
+  },
+  '&:before': {
+    display: 'none',
+  },
+}));
 
 function PostDetail(props: { post: Post }) {
   const { post } = props;
+  const [expanded, setExpanded] = useState<boolean>(true);
 
   return <>
-    <div className={styles['post']}>
-      <div className={styles['post__body']}>
+    <Accordion
+      expanded={expanded}
+      className={styles['post']}
+      onChange={() => setExpanded(!expanded)}
+    >
+      <AccordionSummary className={styles['post__head']} expandIcon={<ExpandMoreIcon />}>
+        <h3 className={styles['post__title']}>{post.tags.length} reaction{post.tags.length > 1 ? 's' : ''}</h3>
+      </AccordionSummary>
+
+      <AccordionDetails className={styles['post__body']}>
         <ul className={styles['reactions']}>
           {post.tags.map(tag => <li key={tag.id} className={styles['reaction']}>
             <div className={styles['reaction__title']}>
@@ -27,9 +50,8 @@ function PostDetail(props: { post: Post }) {
             </div>
           </li>)}
         </ul>
-      </div>
-    </div>
-
+      </AccordionDetails>
+    </Accordion>
   </>
 }
 
